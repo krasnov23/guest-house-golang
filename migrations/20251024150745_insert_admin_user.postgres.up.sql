3e935b3f409bf4be7e785179efede06d2d2f3a26
Boris <krasnov@nethouse.ru>
2025-10-31 13:09:03 +0300

    добавление миграции админа в бд

---
+++ migrations/20251024150745_insert_admin_user.postgres.up.sql
@@ -0,0 +1,11 @@
+INSERT INTO public.users (id, first_name, last_name, email, password, access_level, created_at, updated_at)
+VALUES (
+           1,
+           'admin',
+           'admin',
+           'admin@admin.com',
+           '$2a$10$aUy6qUHNWD/EYu64feSifeduSqezWFvdb5MrVqEFrRxa25bZcK5W2',
+           1,
+           '2025-10-24 14:13:08.000000',
+           '2025-10-24 14:13:12.000000'
+       );
